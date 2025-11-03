452e01eb9b7305ad29b130d0b1754ff9bbf887a0
Yashwant Ramesh Mule <yashwantmule13@gmail.com>
2025-11-03 16:36:05 +0530

    Add files via upload

---
+++ Day 01/Operators/Q2.java
@@ -0,0 +1,14 @@
+public class Q2 {
+    public static void main(String args[]){
+        int x =200, y=50, z=100;
+
+        if(x>y && y>z){
+            System.out.println("Hello");
+        }
+
+        if(z>y && z <x){
+            System.out.println("Hello Java");
+        }
+    }
+
+}
